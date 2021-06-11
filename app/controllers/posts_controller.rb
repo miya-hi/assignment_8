03c4096a0c3aa4f609c5a5becf5be0894b5bb597
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-11 14:57:38 +0900

    編集ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit]
   def index
     @posts = Post.all
   end
@@ -14,6 +14,8 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
   def show
+  end
+  def edit
   end
   private
   def post_params
