370f3bd5f0b73d4e1196671c274a903be1f90bd4
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-11 11:47:42 +0900

    確認ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
     @posts = Post.all
   end
@@ -9,8 +10,13 @@ class PostsController < ApplicationController
     @post = Post.create(post_params)
     redirect_to posts_path
   end
+  def show
+  end
   private
   def post_params
     params.require(:post).permit(:image, :image_cache)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
