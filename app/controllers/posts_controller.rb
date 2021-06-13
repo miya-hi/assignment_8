9951439b0916acba49f81eff3369d0231c244cf6
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-13 19:19:15 +0900

    削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -28,10 +28,14 @@ class PostsController < ApplicationController
   end
   def update
     if @post.update(post_params)
-      redirect_to posts_path, notice: "ブログを編集しました！"
+      redirect_to posts_path, notice: "編集しました"
     else
       render :edit
     end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました"
   end
   private
   def post_params
