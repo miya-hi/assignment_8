e499130f9c9e47d71e65e35d3068e0047ee821c6
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-13 19:26:17 +0900

    Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_084649) do
+ActiveRecord::Schema.define(version: 2021_06_13_102217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_06_10_084649) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
