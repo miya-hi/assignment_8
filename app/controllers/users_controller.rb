196ffd06e6fa9ae936275f83a52e057fb4b9307f
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-14 11:09:49 +0900

    プロフィール編集機能 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -11,9 +12,20 @@ class UsersController < ApplicationController
     end
   end
   def show
-    @user = User.find(params[:id])
+  end
+  def edit
+  end
+  def update
+    if @user.update(user_params)
+      redirect_to action: :show, notice: "編集しました"
+    else
+      render :edit
+    end
   end
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
