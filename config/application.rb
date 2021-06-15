d3858da5b94ccca0bec5a660109685976ade1d66
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-15 09:37:35 +0900

    タイムゾーン東京に設定

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module InstaClone
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
